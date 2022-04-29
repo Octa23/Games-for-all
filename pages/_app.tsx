ed2728988b63b8465b85d7424bfcaf97d4663970
Octa23 <octa233@gmail.com>
2022-04-29 00:55:26 -0300

    Push

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from 'next/app'
 import Header from '../components/Header'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
